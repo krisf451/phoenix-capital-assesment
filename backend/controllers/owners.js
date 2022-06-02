6c9f39c1a457f773656c78344dbb92066222530d
Kristian Fulkerson <kristianf451@gmail.com>
2022-06-01 18:16:32 -0700

    feat: getallowners and getownerbyid routes working and tested

--- backend/controllers/owners.js
+++ backend/controllers/owners.js
@@ -1,14 +1,38 @@
 const Owner = require("../models/owner.js");
 const LandHolding = require("../models/landHolding.js");
 const asyncHandler = require("express-async-handler");
+const mongoose = require("mongoose");
 
-const getAllOwners = (req, res) => {
-  console.log(req.user);
-  res.json({ message: "Get ALL owners" });
-};
-const getOwnerById = (req, res) => {
-  res.json({ message: "Get owner by ID" });
-};
+//GET ALL
+const getAllOwners = asyncHandler(async (req, res) => {
+  const owners = await Owner.find();
+  if (owners) {
+    res.status(200).json({
+      message: "Get all owners successful",
+      data: owners,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Error getting all owners");
+  }
+});
+
+//GET BY ID
+const getOwnerById = asyncHandler(async (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    throw new Error(`No owner with ID ${req.params.id} found`);
+  }
+  const owner = await Owner.findById(req.params.id);
+  if (owner) {
+    res.status(200).json({
+      message: "Get owner by ID successful",
+      data: owner,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Error getting all owners");
+  }
+});
 
 //CREATE NEW OWNER
 const createOwner = asyncHandler(async (req, res) => {
