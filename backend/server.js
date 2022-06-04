e3758735ae97c21015a3f291e039aae162438a63
Kristian Fulkerson <kristianf451@gmail.com>
2022-06-04 01:13:09 -0700

    make it better

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.get("/api/v1", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  res.json({ message: "DEPLOYED SUCCESSFUL" });
+  res.json({ message: "DEPLOYED SUCCESSFUL Test" });
 });
 
 app.use(errorHandler);
